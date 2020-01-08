b1ebe2c706cfdc048b41505771521c7522521273
Raymond Arceneaux <raymond.techcoder@gmail.com>
2020-01-07 20:34:53 -0600

    updated all boards

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+import authData from '../../../helpers/data/authData';
+
+import boardData from '../../../helpers/data/boardData';
+
+import Board from '../Board/Board';
+
+
 import './Home.scss';
 
 class Home extends React.Component {
+  state = {
+    boards: [],
+  }
+
+  componentDidMount() {
+    boardData.getBoardsByUid(authData.getUid())
+      .then((boards) => this.setState({ boards }))
+      .catch((error) => console.error('error', error));
+  }
+
   render() {
-    const boardId = '12345';
     return (
       <div className="home">
         <h1>Home Page</h1>
-        <Link className="btn btn-success" to="/board/new">Create New Board</Link>
-        <Link className="btn btn-secondary" to={`/board/${boardId}`}>Single Board Page</Link>
+        <div className="boards d-flex flex-wrap">
+          {this.state.boards.map((board) => <Board key={board.id} board={board}/>)}
+        </div>
       </div>
     );
   }
--- src/components/pages/SingleBoard/SingleBoard.js
+++ src/components/pages/SingleBoard/SingleBoard.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import boardData from '../../../helpers/data/boardData';
+import pinData from '../../../helpers/data/pinData';
+import Pin from '../shared/Pin/Pin';
 import './SingleBoard.scss';
 
+
 class SingleBoard extends React.Component {
-  render() {
+  state = {
+    board: {},
+    pins: [],
+  }
+
+  getPinData = (boardId) => {
+    pinData.getPinsByBoardId(boardId)
+      .then((pins) => this.setState({ pins }))
+      .catch((error) => console.error('error', error));
+  }
+
+  componentDidMount() {
     const { boardId } = this.props.match.params;
+    boardData.getSingleBoard(boardId)
+      .then((response) => {
+        this.setState({ board: response.data });
+        this.getPinData(boardId);
+      });
+  }
+
+  render() {
+    const { board } = this.state;
     return (
       <div className="singleBoard">
-        <h1>Single Board Page</h1>
-    <h2>Current Board Id is {boardId}</h2>
+  <h1>{board.name}</h1>
+  <p>{board.description}</p>
+    <div className="pins d-flex flex-wrap">
+      { this.state.pins.map((pin) => <Pin key={pin.id} pin={pin}/>)}
+    </div>
       </div>
     );
   }
