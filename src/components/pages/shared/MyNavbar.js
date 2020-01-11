c9b8eac36514a22ef9e0d3ce0513806105b19f3d
Raymond Arceneaux <raymond.techcoder@gmail.com>
2020-01-11 09:08:40 -0600

    fixed issue

--- src/components/pages/shared/MyNavbar.js
+++ src/components/pages/shared/MyNavbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import firebase from 'firebase/app';
 import 'firebase/auth';
@@ -16,18 +17,29 @@ class MyNavbar extends React.Component {
 
   render() {
     const { authed } = this.props;
+    const buildNavbar = () => {
+      if (authed) {
+        return (
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item"><Link className="nav-link" to="/">Boards</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/board/new">New Board</Link></li>
+            <li className="nav-item"><button className="nav-link btn btn-danger" onClick={this.logMeOut}>LOGOUT</button></li>
+           </ul>
+        );
+      }
+
+      return (<ul className="navbar-nav ml-auto"></ul>);
+    };
+
     return (
       <div className="MyNavbar">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="/">Pinterest</a>
+          <Link className="navbar-brand" to="/">Pinterest</Link>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
-            <div class="my-2 mt-lg-0">
-              {authed && (<button className="nav-link btn btn-danger" onClick={this.logMeOut}>Logout</button>)}
-            </div>
+          { buildNavbar() }
           </div>
         </nav>
       </div>
